ffc4517861fc9cd152fd7735dc873c8361ecb080
Chris Austin <acausti5043@gmail.com>
2017-04-19 13:51:50 -0400

    Delete node_setup.sh

--- scripts_and_such/node_setup.sh
+++
@@ -1,8 +0,0 @@
-# create directory for python and openmpi to be installed
-mkdir software
-
-# install openmpi
-ssh -t $1 'sudo mpi-install.sh'
-
-# install python and mpi4py
-ssh -t $1 'sudo -i python-install.sh'
